8cc55dac545d7ff148b029703d830e0bdfd68e15
Karl Culi <kaimax187@gmail.com>
2025-01-15 19:21:47 +0800

    Removed unnecessary css and added the initial template

---
+++ my-weather-app/src/WeatherCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function WeatherCard() {
+  return (
+    <>
+      <div className="container-lg">
+        <h1 className="display-4">Weather App</h1>
+
+        <div className="row justify-content-center text-center p-3 border">
+          <div className="col-5">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo explicabo laudantium, atque pariatur autem fugit quasi repellendus repellat eaque dolore voluptatum perspiciatis nostrum asperiores necessitatibus, cumque error, maiores possimus cum?</p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default WeatherCard;
