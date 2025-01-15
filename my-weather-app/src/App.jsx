bab7e92c2b893bc2d7be93c8b276031ed804678b
Karl Culi <kaimax187@gmail.com>
2025-01-15 19:36:43 +0800

    Added an accordion for user information sheet

--- my-weather-app/src/App.jsx
+++ my-weather-app/src/App.jsx
@@ -1,4 +1,5 @@
 import WeatherCard from "./WeatherCard.jsx"
+import Accordion from "./Accordion.jsx"
 
 function App() {
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <WeatherCard />
+      <Accordion />
     </>
   )
 }
